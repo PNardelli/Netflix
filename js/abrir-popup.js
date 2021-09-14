9b1227f6f4ba150f64d451fc3636f33dddc52827
Pedro Nardelli <pedronardelli@MacBook-Pro-de-Nardelli.local>
2021-09-14 03:12:54 -0300

    inserido video no botao.

---
+++ js/abrir-popup.js
@@ -0,0 +1,12 @@
+function botaoFechar() {
+
+    document.getElementById('popupId').style.display = 'none';
+
+}
+
+function newPopup(){
+
+    
+    document.getElementById('popupId').style.display = 'block';
+    
+}
